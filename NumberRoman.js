5dd46170ae8ba3cd48b72d043604470e91b67e52
bann_perseverance <estebangpa41@gmail.com>
2022-11-07 15:02:16 -0500

    practicas para el document

---
+++ NumberRoman.js
@@ -0,0 +1,30 @@
+// convertir un romano a entero 
+let I = 1
+let V = 5
+let X = 10
+let L = 50
+let C = 100
+let D = 500
+let M = 1000
+var remanToInt = function(s) {
+
+ if (s >= 2 ){
+  var myNum = 
+  s+= s
+ }
+ console.log(`este es el numero ${I}`)
+}
+remanToInt(III);
+
+let a = 3;
+let b = 4;
+let c = 5;
+let s =(a+b+c)/2
+let area =(s*(s-a)*(s-b)*(s-c))**(1/2)
+console.log(area)
+
+//probando foreach
+let frutas = ["manzana" , "melocoton" , "banana"];
+frutas.forEach(function(element,parameter,array){
+ console.log(`el nombre es :  ${element} y el number ${parameter}`)
+})
